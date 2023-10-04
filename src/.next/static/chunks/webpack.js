d258b98c2a0368d16ae406d680794f178a7cc233
jaredper123 <jaredper@byu.edu>
2023-10-03 22:07:08 -0600

    First commits

--- src/.next/static/chunks/webpack.js
+++ src/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "8de8d5e342da46d2"; }
+/******/ 		__webpack_require__.h = function() { return "41d60fe2cb1698b6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/crossword.js
+++ src/pages/crossword.js
@@ -1,16 +1,17 @@
 import Board from "@/components/crossword/board";
 import styles from "@/styles/crossword.module.css"
 
+
 function CrosswordPage() {
 
   return (
     <>
     <h1></h1>
-    <div className={styles.container}>
+      <div className={styles.container}>
       
-      <Board/>
+        <Board/>
       
-    </div>
+      </div> 
     </>
   );
 }
